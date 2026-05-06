c110a228523e6b15a831d70f503be9340985a0b7
Sharat Visweswara <sharat@evocalize.com>
2026-05-05 23:01:08 -0700

    switch theme to sobasic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 export default defineConfig({
 	site: 'https://sharatvisweswara.github.io',
-	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [mdx(), icon(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Sharat Visweswara';
 export const SITE_DESCRIPTION = 'Writing on software, ideas, and whatever else.';
